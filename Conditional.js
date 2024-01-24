9963bb526aeaaa343e7fb1179fde5b55b24bacfd
Md-Golam-Mostafa <gmshakilcse@gmail.com>
2024-01-24 15:33:10 +0600

    Variable and Conditional added

---
+++ Conditional.js
@@ -0,0 +1,19 @@
+var bat = 2;
+
+if (2=== bat) {
+    console.log("I have 2 bat");
+}
+else {
+    console.log("I have 3 bat");
+}
+var magnet =2
+
+if (2=== magnet) {
+    console.log("I have 2 magnet");
+}
+ else {
+    console.log("I have 3 magnet");
+ }   
+
+
+
---
+++ Variable.js
@@ -0,0 +1,31 @@
+var sleep = "Saad is Sleeping now...";
+console.log(sleep);
+var book = "I have new Book"
+console.log(book);
+var breakfast = "I have breakfast at morning"
+console.log(breakfast);
+var saad = "saad is my brother"
+console.log(saad);
+var eveningSnack = "I am going to have evening snack"
+console.log(eveningSnack);
+var eveningSnack = "I haved evening snack"
+console.log(eveningSnack);
+var sleep = "I will go to sleep"
+console.log(sleep);
+var dinner = "I don't have dinner I have bread and nutela instead of dinner"
+console.log(dinner);
+var sleep = "I wake up from sleep"
+console.log(sleep);
+var brush = "I brush my teeth"
+console.log(brush);
+
+var ball = 5;
+var bat = 3;
+
+console.log(ball + bat);
+
+
+
+
+
+
