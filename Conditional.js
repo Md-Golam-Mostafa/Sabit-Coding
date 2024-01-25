dfc9cc1a007737fea542274bc59361d04bb61ec5
Md-Golam-Mostafa <gmshakilcse@gmail.com>
2024-01-25 11:40:38 +0600

    conditional code updated

--- Conditional.js
+++ Conditional.js
@@ -1,19 +1,85 @@
 var bat = 2;
 
-if (2=== bat) {
+if (2 === bat) {
     console.log("I have 2 bat");
 }
 else {
     console.log("I have 3 bat");
 }
-var magnet =2
+var magnet = 2
 
-if (2=== magnet) {
+if (2 === magnet) {
     console.log("I have 2 magnet");
 }
- else {
+else {
     console.log("I have 3 magnet");
- }   
+}
+var eyeglasses = 0
+
+if (2 === eyeglasses) {
+    console.log("I have 0 eyeglasses");
+}
+else {
+    console.log; ("I have 1 eyeglasses");
+}
+
+var guests = 3
+
+if (3 === guests) {
+    console.log("I have 3 guests");
+}
+
+else {
+    console.log("I have 2 guests");
+}
+
+
+var book = 6
+var fundBook = 6
+if (fundBook === book) {
+    console.log("Yes,Right");
+}
+else {
+    console.log("No,that's not true");
+}
+var book = 6
+fundBook = 6
+if (fundBook === book) {
+    console.log("yes,that's true");
+}
+else {
+    console.log("no,that's false");
+}
+var bottle = 5
+fundBottle = 5
+if (fundBottle === bottle) {
+    console.log("yes,that's true");
+}
+else {
+    console.log("no,that's false");
+}
+var light = 3
+fundLight = 9
+if (fundLight === light) {
+    console.log("yes,that's true");
+}
+else {
+    console.log("no,that's false");
+}
+var picture = 4
+fundPicture = 1234
+if (fundPicture === picture) {
+    console.log("yes,that's true");
+}
+else {
+    console.log("no that,s false");
+}
+
+var picture = (fundPicture >= 4) ? console.log("yes") : console.log("no");;
+
+
+
+
 
 
 
